a4c2b9fcca8c065bd15206abaa44e837629f5d96
Keon-Carrillo <kpcarrillo2030@clc.pshs.edu.ph>
2026-08-15 17:49:01 +0800

    Add Chinese zodiac sign determination based on birth year

--- Q1/q1_sg2_a3.py
+++ Q1/q1_sg2_a3.py
@@ -1,1 +1,33 @@
+'''
+Keon P. Carrillo
+9-Samat
+'''
+baseYear = 1900
+birthYear = int(input("Enter your birth year: "))
 
+if 1900 <= birthYear <= 1912:
+    print("Your Chinese zodiac sign is: Rat (鼠 / Shǔ)")
+elif 1912 < birthYear <= 1924:
+    print("Your Chinese zodiac sign is: Ox (牛 / Niú)")
+elif 1924 < birthYear <= 1936:
+    print("Your Chinese zodiac sign is: Tiger (虎 / Hǔ)")
+elif 1936 < birthYear <= 1948:
+    print("Your Chinese zodiac sign is: Rabbit (兔 / Tù)")
+elif 1948 < birthYear <= 1960:
+    print("Your Chinese zodiac sign is: Dragon (龙 / Lóng)")
+elif 1960 < birthYear <= 1972:
+    print("Your Chinese zodiac sign is: Snake (蛇 / Shé)")
+elif 1972 < birthYear <= 1984:
+    print("Your Chinese zodiac sign is: Horse (马 / Mǎ)")
+elif 1984 < birthYear <= 1996:
+    print("Your Chinese zodiac sign is: Goat (羊 / Yáng)")
+elif 1996 < birthYear <= 2008:
+    print("Your Chinese zodiac sign is: Monkey (猴 / Hóu)")
+elif 2008 < birthYear <= 2020:
+    print("Your Chinese zodiac sign is: Rooster (鸡 / Jī)")
+elif 2020 < birthYear <= 2032:
+    print("Your Chinese zodiac sign is: Dog (狗 / Gǒu)")
+elif 2032 < birthYear <= 2044:
+    print("Your Chinese zodiac sign is:  Pig (猪 / Zhū)")
+else:
+    print("Invalid input. Birth years should not be earlier than 1900.")
