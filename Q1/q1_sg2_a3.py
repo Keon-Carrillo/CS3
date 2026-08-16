9911dd6bb69da2fb17072f79aaa3bf95e11378f8
Keon-Carrillo <kpcarrillo2030@clc.pshs.edu.ph>
2026-08-16 10:27:09 +0800

    Update q1_sg2_a3.py

--- Q1/q1_sg2_a3.py
+++ Q1/q1_sg2_a3.py
@@ -16,39 +16,39 @@ else:
     #Determines how many years have passed from 1900, up to your birth year
     yearsPassed = (birthYear - 1900)
 
-    #Modolus uses division to check how many times the zodiacs (12) have been fully cycled and grabs the remainder to determine your birth year's place
-    if yearsPassed % 12 == 1:
+    #Modulus uses division to check how many times the zodiacs (12) have been fully cycled and grabs the remainder to determine your birth year's place
+    if yearsPassed % 12 == 0:
         print("Your Chinese zodiac sign is: Rat (鼠 / Shǔ)")
 
-    elif yearsPassed % 12 == 2:
+    elif yearsPassed % 12 == 1:
         print("Your Chinese zodiac sign is: Ox (牛 / Niú)")
 
-    elif yearsPassed % 12 == 3:
+    elif yearsPassed % 12 == 2:
         print("Your Chinese zodiac sign is: Tiger (虎 / Hǔ)")
 
-    elif yearsPassed % 12 == 4:
+    elif yearsPassed % 12 == 3:
         print("Your Chinese zodiac sign is: Rabbit (兔 / Tù)")
 
-    elif yearsPassed % 12 == 5:
+    elif yearsPassed % 12 == 4:
         print("Your Chinese zodiac sign is: Dragon (龙 / Lóng)")
 
-    elif yearsPassed % 12 == 6:
+    elif yearsPassed % 12 == 5:
         print("Your Chinese zodiac sign is: Snake (蛇 / Shé)")
 
-    elif yearsPassed % 12 == 7:
+    elif yearsPassed % 12 == 6:
         print("Your Chinese zodiac sign is: Horse (马 / Mǎ)")
 
-    elif yearsPassed % 12 == 8:
+    elif yearsPassed % 12 == 7:
         print("Your Chinese zodiac sign is: Goat (羊 / Yáng)")
 
-    elif yearsPassed % 12 == 9:
+    elif yearsPassed % 12 == 8:
         print("Your Chinese zodiac sign is: Monkey (猴 / Hóu)")
 
-    elif yearsPassed % 12 == 10:
+    elif yearsPassed % 12 == 9:
         print("Your Chinese zodiac sign is: Rooster (鸡 / Jī)")
 
-    elif yearsPassed % 12 == 11:
+    elif yearsPassed % 12 == 10:
         print("Your Chinese zodiac sign is: Dog (狗 / Gǒu)")
 
-    elif yearsPassed % 12 == 0:
+    elif yearsPassed % 12 == 11:
         print("Your Chinese zodiac sign is: Pig (猪 / Zhū)")
