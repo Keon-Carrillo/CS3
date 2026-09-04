7e6f37524bce625717491dfe04e97dd27f326d62
Keon-Carrillo <kpcarrillo2030@clc.pshs.edu.ph>
2026-09-04 20:16:05 +0800

    Add files via upload

---
+++ Q1/class_test_car_game.py
@@ -0,0 +1,29 @@
+class Car:
+    #constructor
+    def __init__(self, brand, model, battery=35):
+        self.brand = brand
+        self.model = model
+        self.battery = battery
+
+    def go(self, distance):
+        self.battery -= distance/20
+        print("You have travelled", distance)
+        print("you have", self.battery, "wH left")
+
+    def charge(self, wH):
+        self.battery += wH
+        print("You reccharged with,", wH, "wH")
+
+car = Car("Geely", "EX5")
+
+while car.battery > 0:
+    act = input("What do we do? (g or c) ")
+    if act == "g":
+        distance = int(input("How far? "))
+        car.go(distance)
+    elif act == "c":
+        wH = int(input("How much to charge? "))
+        car.charge(wH)
+    else:
+        print("Invalid action")
+print("Game over, you ran out of battery")
